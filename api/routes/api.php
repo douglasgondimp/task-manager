fced7085544fa5dee23acd5083a18d22d196162a
Douglas Gondim <douglasgondimp@gmail.com>
2026-07-28 19:05:11 -0300

    feat(api): adiciona rotas para os projetos e tarefas

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,8 +1,31 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ProjectTaskController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('throttle:api')->group(function () {
+    Route::controller(ProjectController::class)
+        ->prefix('projects')
+        ->group(function () {
+            Route::get('/', 'index');
+            Route::post('/', 'store');
+            Route::patch('/{project}', 'update');
+        });
+
+    Route::controller(ProjectTaskController::class)
+        ->prefix('projects/{projetct}')
+        ->group(function () {
+            Route::get('/tasks', 'index');
+            Route::post('/tasks', 'store');
+        });
+
+    Route::controller(TaskController::class)
+        ->prefix('tasks')
+        ->group(function () {
+            Route::patch('/{task}', 'update');
+            Route::delete('/{task}', 'delete');
+        });
+});
