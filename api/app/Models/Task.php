ea44a1bde5c042311bfeadef8abadb5b69e0e8ea
Douglas Gondim <douglasgondimp@gmail.com>
2026-07-29 20:20:10 -0300

    refactor(api): Troca o uso do Query Bulder para Eloquent Builder na model Task

--- api/app/Models/Task.php
+++ api/app/Models/Task.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use App\Enums\TaskPriority;
 use App\Enums\TaskStatus;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Query\Builder;
 
 class Task extends Model
 {
