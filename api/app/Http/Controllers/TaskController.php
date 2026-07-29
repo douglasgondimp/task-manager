78290ee05e53066971719230c1b6b0db20088228
Douglas Gondim <douglasgondimp@gmail.com>
2026-07-29 12:34:34 -0300

    feat(api): adiciona script para deletar uma task

--- api/app/Http/Controllers/TaskController.php
+++ api/app/Http/Controllers/TaskController.php
@@ -46,8 +46,12 @@ class TaskController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Task $task)
     {
-        //
+        $task->delete();
+
+        return response()->json([
+            "message" => "Task deletada com sucesso."
+        ]);
     }
 }
