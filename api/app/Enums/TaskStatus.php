081d3be040b1861711416686abe13bbb4a713aa2
Douglas Gondim <douglasgondimp@gmail.com>
2026-07-29 11:49:27 -0300

    feat(api): cria classe label nas enums TaskPriority e TaskStatus

--- api/app/Enums/TaskStatus.php
+++ api/app/Enums/TaskStatus.php
@@ -7,4 +7,13 @@ enum TaskStatus: string
     case Todo       = 'todo';
     case InProgress = 'in_progress';
     case Done       = 'done';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::Todo       => 'A fazer',
+            self::InProgress => 'Em desenvolvimento',
+            self::Done       => 'Completo'
+        };
+    }
 }
