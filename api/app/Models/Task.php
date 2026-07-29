e3b87dee7b69bd645589b1e80ce49d5ded9be288
Douglas Gondim <douglasgondimp@gmail.com>
2026-07-29 12:17:40 -0300

    feat(api): adiciona soft delete para tasks

--- api/app/Models/Task.php
+++ api/app/Models/Task.php
@@ -7,11 +7,12 @@ use App\Enums\TaskStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Query\Builder;
 
 class Task extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'project_id',
