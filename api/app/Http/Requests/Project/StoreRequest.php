3d4bcf7957b44ffa56b5b7407c8e81c96602805b
Douglas Gondim <douglasgondimp@gmail.com>
2026-07-29 10:49:07 -0300

    feat(api): cria classe de requisição ao criar um novo projeto

---
+++ api/app/Http/Requests/Project/StoreRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Project;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [
+            'name'        => ['required', 'string', 'max:50'],
+            'description' => ['nullable', 'string'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'O nome é obrigatório.',
+            'name.max'      => 'O nome excede o limite permitido. Máx: :value'
+        ];
+    }
+}
